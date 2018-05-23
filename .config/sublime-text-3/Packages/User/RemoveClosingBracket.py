878e883bc7246d865f94a5d4bf4e3a9de5263a7c
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2018-05-23 16:04:39 +0200

    Sublime Text Autoclosing-everything

--- .config/sublime-text-3/Packages/User/RemoveClosingBracket.py
+++ .config/sublime-text-3/Packages/User/RemoveClosingBracket.py
@@ -8,26 +8,32 @@ class RemoveClosingBracket(sublime_plugin.TextCommand):
 		curr_point = self.view.sel()[0]
 		region_line = self.view.line(curr_point)
 		line = self.view.substr(region_line)
-		
+
 		row, col = self.view.rowcol(curr_point.begin())
 
-		try:
-			i = col
-			nb_brackets = 1
+		if col >= 1 and line[col-1] in '([{':
+			sign = line[col-1]
 
-			while nb_brackets > 0:
-				if line[i] == '(':
-					nb_brackets += 1
-				elif line[i] == ')':
-					nb_brackets -= 1
-				i += 1
+			close_sign = sign.translate(str.maketrans('([{', ')]}'))
+			print(close_sign)
+			
+			try:
+				i = col
+				nb_brackets = 1
 
-			index_closing_bracket = self.view.text_point(row, i - 1)
-			region = sublime.Region(index_closing_bracket, index_closing_bracket+1)
-			self.view.erase(edit, region)
+				while nb_brackets > 0:
+					if line[i] == sign:
+						nb_brackets += 1
+					elif line[i] == close_sign:
+						nb_brackets -= 1
+					i += 1
 
-		except (ValueError, IndexError):
-			pass
+				index_closing_bracket = self.view.text_point(row, i - 1)
+				region = sublime.Region(index_closing_bracket, index_closing_bracket+1)
+				self.view.erase(edit, region)
+
+			except (ValueError, IndexError):
+				pass
 
 		self.view.run_command('left_delete')
 
