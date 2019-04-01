203da8ddb752596d4617f93ca1ddef7e6a1b099e
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2019-04-01 10:33:40 +0200

    Fix sublime (toggle_)debug_comment

--- .config/sublime-text-3/Packages/User/ToggleDebugComment.py
+++ .config/sublime-text-3/Packages/User/ToggleDebugComment.py
@@ -12,11 +12,11 @@ class ToggleDebugCommentCommand(sublime_plugin.TextCommand):
 		if action == "comment" or action == "toggle":
 			for area in areas_to_comment[::-1]:
 				text = self.view.substr(area)
-				modified_text = re.sub(r'^(\s*)(.*# debug)\s*$', r'\1# \2', text)
+				modified_text = re.sub(r'^(\s*)(.*# debug)\h*$', r'\1# \2', text)
 				self.view.replace(edit, area, modified_text)
 
 		if action == "uncomment" or action == "toggle":
 			for area in areas_to_uncomment[::-1]:
 				text = self.view.substr(area)
-				modified_text = re.sub(r'^(\s*)# (.*# debug)\s*$', r'\1\2', text)
+				modified_text = re.sub(r'^(\s*)# (.*# debug)\h*$', r'\1\2', text)
 				self.view.replace(edit, area, modified_text)
