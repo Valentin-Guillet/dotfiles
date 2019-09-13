66cdd5aca6810867d529c712fd463d2d860d68d3
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2019-09-13 17:14:36 +0200

    Sublime RemoveDebug on selection + tmux kill-all except

--- .config/sublime-text-3/Packages/User/RemoveDebug.py
+++ .config/sublime-text-3/Packages/User/RemoveDebug.py
@@ -4,7 +4,26 @@ import re
 
 
 class RemoveDebugCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		region = sublime.Region(0, self.view.size())
-		new_text = re.sub(".*# debug\n", '', self.view.substr(region))
-		self.view.replace(edit, region, new_text)
+
+    def run(self, edit):
+
+        selections = list(self.view.sel())
+
+        # If no specific area is selected, select every line with '# debug'
+        if not selections or (len(selections) == 1 and len(selections[0]) == 0):
+            lines = self.view.find_all(r'^.*# debug\h*\n')
+
+        # Else, select only the lines within the selections
+        else:
+            lines = []
+            for sel in selections:
+                for line in self.view.lines(sel):
+                    line = self.view.full_line(line)  # selects also the trailing newline character
+                    text = self.view.substr(line)
+                    match = re.match(r'^.*# debug\h*\n', text)
+
+                    if match and line not in lines:
+                        lines.append(line)
+
+        for line in lines[::-1]:
+            self.view.erase(edit, line)
--- .config/sublime-text-3/Packages/User/ToggleDebugComment.py
+++ .config/sublime-text-3/Packages/User/ToggleDebugComment.py
@@ -5,17 +5,46 @@ import re
 
 class ToggleDebugCommentCommand(sublime_plugin.TextCommand):
 
-    def is_selected(self, area):
+    def run(self, edit, action):
+
         selections = list(self.view.sel())
-        if len(selections) == 1 and len(selections[0]) == 0:
-            return True
 
-        for sel in selections:
-            for line in self.view.lines(sel):
-                if line.contains(area):
-                    return True
+        # If no specific area is selected, select every line with '# debug'
+        if not selections or (len(selections) == 1 and len(selections[0]) == 0):
+            areas_to_comment = self.view.find_all(r'^(\h*)([^#\n]*# debug)\h*$')
+            areas_to_uncomment = self.view.find_all(r'^(\h*)# (.*# debug)\h*$')
 
-        return False
+        # Else, select only the lines within the selections
+        else:
+            areas_to_comment = []
+            areas_to_uncomment = []
+            for sel in selections:
+                for line in self.view.lines(sel):
+                    text = self.view.substr(line)
+
+                    # Must the line be commented...
+                    match_comment = re.match(r'^(\h*)([^#\n]*# debug)\h*$', text)
+                    if match_comment and line not in areas_to_comment:
+                        areas_to_comment.append(line)
+
+                    # ...or uncommented ?
+                    match_uncomment = re.match(r'^(\h*)# (.*# debug)\h*$', text)
+                    if match_uncomment and line not in areas_to_uncomment:
+                        areas_to_uncomment.append(line)
+
+        if action == "comment":
+            for area in areas_to_comment[::-1]:
+                self.toggle_line(edit, area, 'comment')
+
+        elif action == "uncomment":
+            for area in areas_to_uncomment[::-1]:
+                self.toggle_line(edit, area, 'uncomment')
+
+        elif action == "toggle":
+            areas_to_toggle = sorted(areas_to_comment + areas_to_uncomment)
+            for area in areas_to_toggle[::-1]:
+                action = 'comment' if area in areas_to_comment else 'uncomment'
+                self.toggle_line(edit, area, action)
 
     def toggle_line(self, edit, area, action):
         text = self.view.substr(area)
@@ -27,25 +56,3 @@ class ToggleDebugCommentCommand(sublime_plugin.TextCommand):
             modified_text = re.sub(r'^(\h*)# (.*# debug)\h*$', r'\1\2', text)
 
         self.view.replace(edit, area, modified_text)
-
-    def run(self, edit, action):
-
-        areas_to_comment = self.view.find_all('^(\h*)([^#\n]*# debug)\h*$')
-        areas_to_uncomment = self.view.find_all('^(\h*)# (.*# debug)\h*$')
-
-        if action == "comment":
-            for area in areas_to_comment[::-1]:
-                if self.is_selected(area):
-                    self.toggle_line(edit, area, 'comment')
-
-        elif action == "uncomment":
-            for area in areas_to_uncomment[::-1]:
-                if self.is_selected(area):
-                    self.toggle_line(edit, area, 'uncomment')
-
-        elif action == "toggle":
-            areas_to_toggle = sorted(areas_to_comment + areas_to_uncomment)
-            for area in areas_to_toggle[::-1]:
-                if self.is_selected(area):
-                    action = 'comment' if area in areas_to_comment else 'uncomment'
-                    self.toggle_line(edit, area, action)
