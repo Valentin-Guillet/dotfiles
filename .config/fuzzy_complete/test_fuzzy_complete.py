393c8d9c9daf18d34cf51d8bf352c3315394c5bf
Valentin Guillet <guillet.valentin95240@gmail.com>
2023-05-09 17:29:23 +0200

    Test fake file structure

--- .config/fuzzy_complete/test_fuzzy_complete.py
+++ .config/fuzzy_complete/test_fuzzy_complete.py
@@ -1,16 +1,59 @@
 
 import io
 import os
+import shutil
 import sys
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
 import fuzzy_complete
 
+FUZZTEST_DIR = Path(os.environ["HOME"]) /"FuzzTest"
 
-def define_test(cls, name, path, args, target):
+def setUpModule():
+    files = [
+        FUZZTEST_DIR/"Documents",
+
+        FUZZTEST_DIR/"Documents"/".hidden",
+
+        FUZZTEST_DIR/"Documents"/"Misc",
+        FUZZTEST_DIR/"Documents"/"Misc"/"dir1_file.txt",
+        FUZZTEST_DIR/"Documents"/"Misc"/"dependency",
+        FUZZTEST_DIR/"Documents"/"Misc"/"dependency"/"dep 1.txt",
+        FUZZTEST_DIR/"Documents"/"Misc"/"dependency"/"dep 2.txt",
+        FUZZTEST_DIR/"Documents"/"Misc"/"dependency"/"dep 3.txt",
+        FUZZTEST_DIR/"Documents"/"Misc"/"subdir",
+        FUZZTEST_DIR/"Documents"/"Misc"/"subdir"/"subfile1.log",
+        FUZZTEST_DIR/"Documents"/"Misc"/"subdir"/"subfile2.log",
+        FUZZTEST_DIR/"Documents"/"program",
+        FUZZTEST_DIR/"Documents"/"program"/"prog.cpp",
+        FUZZTEST_DIR/"Documents"/"program"/"prog.hpp",
+        FUZZTEST_DIR/"Documents"/"project",
+        FUZZTEST_DIR/"Documents"/"project"/"package.py",
+        FUZZTEST_DIR/"Documents"/"project"/"utils.py",
+
+        FUZZTEST_DIR/"Downloads",
+        FUZZTEST_DIR/"Downloads"/"book.epub",
+        FUZZTEST_DIR/"Downloads"/"video.mp4",
+
+        FUZZTEST_DIR/"Videos",
+        FUZZTEST_DIR/"Videos"/"movie 1.mkv",
+        FUZZTEST_DIR/"Videos"/"movie 2.mkv",
+    ]
+
+    for path in files:
+        if str(path).startswith(".") or not path.suffix:
+            path.mkdir(parents=True)
+        else:
+            path.touch()
+
+def tearDownModule():
+    shutil.rmtree(FUZZTEST_DIR, ignore_errors=True)
+
+def define_test(cls, name, args, target):
     def _inner_test(self):
-        os.chdir(path)
+        os.chdir(FUZZTEST_DIR)
         full_args = self.common_args + args
         with patch.object(sys, "argv", full_args):
             fuzzy_complete.main()
@@ -31,44 +74,12 @@ class CdTest(unittest.TestCase):
     def tearDown(self):
         sys.stdout = sys.__stdout__
 
-#     def test_doc_aoc_split(self):
-#         os.chdir("/home/valentin")
-#         args = self.common_args + ["doc", "aoc"]
-#         with patch.object(sys, "argv", args):
-#             fuzzy_complete.main()
-
-#         output = self.captured_output.getvalue()
-#         self.assertEqual(output, "AdventOfCode\n")
-
-#     def test_doc_aoc_joined(self):
-#         os.chdir("/home/valentin")
-#         args = self.common_args + ["doc/aoc"]
-#         with patch.object(sys, "argv", args):
-#             fuzzy_complete.main()
-
-#         output = self.captured_output.getvalue()
-#         self.assertEqual(output, "Documents/AdventOfCode\n")
-
-#     def test_root_split(self):
-#         os.chdir("/home/valentin")
-#         args = self.common_args + ["doc/aoc"]
-#         with patch.object(sys, "argv", args):
-#             fuzzy_complete.main()
-
-#         output = self.captured_output.getvalue()
-#         self.assertEqual(output, "Documents/AdventOfCode\n")
-
-    # def test_cd_tilde(self):
-    #     os.chdir("/home/valentin")
-    #     args = self.common_args + ["~", ""]
-    #     with patch.object(sys, "argv", args):
-    #         fuzzy_complete.main()
-
-    #     print(self.captured_output.getvalue())
-
-
 if __name__ == "__main__":
-    define_test(CdTest, "doc", "/home/valentin", ["doc"], "Documents\n")
-    define_test(CdTest, "doc_aoc_split", "/home/valentin", ["doc", "aoc"], "AdventOfCode\n")
-    define_test(CdTest, "doc_aoc_joined", "/home/valentin", ["doc/aoc"], "Documents/AdventOfCode\n")
+    define_test(CdTest, "simple", ["Doc"], "Documents\n")
+    define_test(CdTest, "skip_letters", ["dw"], "Downloads\n")
+    define_test(CdTest, "multiple", ["do"], "Documents\nDownloads\n\u1160\n")
+    define_test(CdTest, "successive", ["do", "proj"], "project\n")
+    define_test(CdTest, "successive_joined", ["do/proj"], "Documents/project\n")
+    define_test(CdTest, "hidden", ["do", "hid"], ".hidden\n")
+    define_test(CdTest, "match_chars", ["dmd"], "Documents/Misc/dependency\n")
     unittest.main()
