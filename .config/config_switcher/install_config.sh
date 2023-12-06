ccb01f40d33d9ed68d9409723a1beda431dc7a67
Val95240 <guillet.valentin95240@gmail.com>
2023-12-06 20:22:30 +0100

    Config_switcher: complete update

--- .config/config_switcher/install_config.sh
+++ .config/config_switcher/install_config.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-mkdir backup
+mkdir -p backup
 
-while read file
-do
-    mkdir -p backup/$(dirname "$file")
-    if [ -f "$file" ]
-    then
-        cp "$file" backup/
-    elif [ -d "$file" ]
-    then
-        cp -r "$file" backup/
-    fi
-done < <(sed -e "/^\s*#.*$/d" -e "/^\s*$/d" list_files)
+./toggle_config backup
 
-sed -i "s;^\(\s\+\)export CONFIG_DIR=.*;\1export CONFIG_DIR=$(realpath .);" toggle_config
-
+echo ""
 echo "Create a symbolic link in a directory from PATH to $(realpath toggle_config)"
 echo "For instance :"
-echo "    ln -s $(realpath toggle_config) ~/.local/bin/val"
+echo "    ln -s $(realpath toggle_config) ~/.local/bin/config_switch"
 
