3808a6e1997e6a9702f6e1381a876203233fdc34
Val95240 <guillet.valentin95240@gmail.com>
2024-01-26 19:09:45 +0100

    Ptpy: add pdir2 by default

--- .config/python/pythonrc.py
+++ .config/python/pythonrc.py
@@ -1,35 +1,47 @@
-import atexit
-import os
-import readline
+
 import sys
-from pathlib import Path
 
-
-def write_history(history_path):
+def setup_history_path():
+    import atexit
+    import os
     import readline
+    import sys
     from pathlib import Path
+
+
+    def write_history(history_path):
+        import readline
+        from pathlib import Path
+        try:
+            Path(history_path).parent.mkdir(parents=True, exist_ok=True)
+            readline.write_history_file(history_path)
+        except OSError:
+            pass
+
+    if sys.version_info > (3, 6):
+        cache_dir = Path(os.environ.get("XDG_CACHE_HOME") or "~/.cache").expanduser()
+        history_file = cache_dir / "python" / "history"
+
+        readline.set_history_length(10000)
+        try:
+            readline.read_history_file(history_file)
+        except FileNotFoundError:
+            pass
+
+        # Prevents creation of default history if custom is empty
+        if readline.get_current_history_length() == 0:
+            readline.add_history("# History creation")
+
+        atexit.register(write_history, history_file)
+
+
+if "ptpython" not in sys.modules:
+    setup_history_path()
+else:
     try:
-        Path(history_path).parent.mkdir(parents=True, exist_ok=True)
-        readline.write_history_file(history_path)
-    except OSError:
+        import pdir as dir
+    except ImportError:
         pass
 
-if sys.version_info > (3, 6):
-    cache_dir = Path(os.environ.get("XDG_CACHE_HOME") or "~/.cache").expanduser()
-    history_file = cache_dir / "python" / "history"
-
-    readline.set_history_length(10000)
-    try:
-        readline.read_history_file(history_file)
-    except FileNotFoundError:
-        pass
-
-    # Prevents creation of default history if custom is empty
-    if readline.get_current_history_length() == 0:
-        readline.add_history("# History creation")
-
-    atexit.register(write_history, history_file)
-
-del atexit, os, readline, sys
-del cache_dir, history_file
-del Path
+del sys
+del setup_history_path
