18bad0153a349d481f8e9ccd89e44e41f21fad7a
Val95240 <guillet.valentin95240@gmail.com>
2024-02-29 15:03:07 +0100

    Pythonrc: fix linter

--- .config/python/pythonrc.py
+++ .config/python/pythonrc.py
@@ -1,11 +1,13 @@
 
+import contextlib
 import sys
+
 
 def setup_history_path():
     import atexit
+    import contextlib
     import os
     import readline
-    import sys
     from pathlib import Path
 
 
@@ -18,30 +20,27 @@ def setup_history_path():
         except OSError:
             pass
 
-    if sys.version_info > (3, 6):
-        cache_dir = Path(os.environ.get("XDG_CACHE_HOME") or "~/.cache").expanduser()
-        history_file = cache_dir / "python" / "history"
+    cache_dir = Path(os.environ.get("XDG_CACHE_HOME") or "~/.cache").expanduser()
+    history_file = cache_dir / "python" / "history"
 
-        readline.set_history_length(10000)
-        try:
-            readline.read_history_file(history_file)
-        except FileNotFoundError:
-            pass
+    readline.set_history_length(10000)
+    with contextlib.suppress(FileNotFoundError):
+        readline.read_history_file(history_file)
 
-        # Prevents creation of default history if custom is empty
-        if readline.get_current_history_length() == 0:
-            readline.add_history("# History creation")
+    # Prevents creation of default history if custom is empty
+    if readline.get_current_history_length() == 0:
+        readline.add_history("# History creation")
 
-        atexit.register(write_history, history_file)
+    atexit.register(write_history, history_file)
 
 
 if "ptpython" not in sys.modules:
     setup_history_path()
 else:
-    try:
-        import pdir as dir
-    except ImportError:
-        pass
+    with contextlib.suppress(ImportError):
+        import pdir as dir     # noqa
 
+
+del contextlib
 del sys
 del setup_history_path
