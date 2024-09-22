acc0f39cafff7c9e1c166972611810baa528edf1
Val95240 <guillet.valentin95240@gmail.com>
2024-09-22 23:57:31 +0200

    Use NeoCodeium plugin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
 
     -- import any extras modules here
 
-    { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -5,12 +5,42 @@ return {
 
     opts = function(_, opts)
       local cmp = require("cmp")
+      local neocodeium = require("neocodeium")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-B>"] = cmp.config.disable,
         ["<C-F>"] = cmp.config.disable,
         ["<C-K>"] = cmp.mapping.scroll_docs(-4),
         ["<C-J>"] = cmp.mapping.scroll_docs(4),
+        ["<Tab>"] = cmp.mapping({
+          i = function(fallback)
+            if neocodeium.visible() then -- accept completion
+              neocodeium.accept()
+            elseif cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else -- send "<Tab>" through
+              fallback()
+            end
+          end,
+        })
       })
+    end,
+  },
+
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+      local neocodeium = require("neocodeium")
+      neocodeium.setup({manual = true})
+
+      -- create an autocommand which closes cmp when ai completions are displayed
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeoCodeiumCompletionDisplayed",
+        callback = function() require("cmp").abort() end
+      })
+
+      vim.keymap.set("i", "<M-]>", function() neocodeium.cycle_or_complete(1) end)
+      vim.keymap.set("i", "<M-[>", function() neocodeium.cycle_or_complete(-1) end)
     end,
   },
 
