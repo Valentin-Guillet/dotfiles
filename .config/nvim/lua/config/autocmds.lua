e315e71283ba794909e0954b40a86d629ef84c6b
Valentin-Guillet <guillet.valentin95240@gmail.com>
2024-11-12 17:16:10 +0100

    Nvim: nohl on cursor move

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "text", "markdown", "sh", "bib", "tex" },
 	callback = function() vim.opt_local.iskeyword:append("-") end,
 })
+
+vim.api.nvim_create_autocmd('CursorMoved', {
+	callback = function ()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function() vim.cmd.nohlsearch() end)
+		end
+	end
+})
