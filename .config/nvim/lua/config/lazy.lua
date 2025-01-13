2ffa965c2de21f915b7fad9f4b9498da07ebb4a5
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-01-13 19:26:31 +0100

    Nvim add Projects

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
+		{ import = "lazyvim.plugins.extras.util.project" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
