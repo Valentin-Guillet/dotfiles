8ae6e14ba43e0906688d0cae09c1ae0b8c7cf3b9
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-01-16 11:35:29 +0100

    Nvim: fix <C-k> in insertmode for doc scroll

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,4 +18,13 @@ return {
 			},
 		},
 	},
+
+	{
+		"neovim/nvim-lspconfig",
+		opts = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			-- Remove to let <C-k> scroll documentation in Noice
+			keys[#keys + 1] = { "<C-k>", false, mode = "i" }
+		end
+	},
 }
