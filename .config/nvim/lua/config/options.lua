85b2394a77e6af45145213f5a19db9421e11f380
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-02-05 18:24:28 +0100

    Nvim+Git: use histogram diff algorithm

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,6 +7,8 @@ vim.opt.sidescrolloff = 5
 
 vim.opt.gdefault = true
 
+vim.opt.diffopt = "internal,filler,closeoff,algorithm:histogram,indent-heuristic,linematch:60"
+
 vim.g.autoformat = false
 
 vim.g.snacks_animate = false
