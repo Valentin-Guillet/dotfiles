2ee8b86057340684f7a5c1e832956be7e9cfd5d1
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-03-19 13:44:34 +0100

    Nvim: full filepath in status line

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -24,6 +24,20 @@ return {
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			sections = {
+				lualine_c = {
+					LazyVim.lualine.root_dir(),
+					{
+						"diagnostics",
+						symbols = {
+							error = LazyVim.config.icons.diagnostics.Error,
+							warn = LazyVim.config.icons.diagnostics.Warn,
+							info = LazyVim.config.icons.diagnostics.Info,
+							hint = LazyVim.config.icons.diagnostics.Hint,
+						},
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ LazyVim.lualine.pretty_path({ length = 0 }) },
+				},
 				lualine_y = { "%3l/%L %3c|" },
 			},
 		},
