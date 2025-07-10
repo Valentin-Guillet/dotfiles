7e755bde35b700e368172b3d813fa39823a138cf
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-07-10 14:42:04 +0200

    Nvim: pyright add editable pip pkgs to extraPaths

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,13 +3,6 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				pyright = {
-					settings = {
-						python = {
-							pythonPath = vim.env.HOME .. "/.local/venvs/base/bin/python",
-						},
-					},
-				},
 				clangd = {
 					init_options = {
 						fallbackFlags = { "-std=c++23" },
