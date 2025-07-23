ef073869cf69382908f32f684a5eb7e7620624e8
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-07-23 10:14:52 +0200

    Fuzzycd: fix file exclusion on char search with multiple patterns

--- .config/fuzzy_complete/fuzzy_lib.py
+++ .config/fuzzy_complete/fuzzy_lib.py
@@ -261,7 +261,11 @@ def matches_for_path(
         if allow_by_char and is_first_word_path and not found_dirs:
             if pattern.startswith(".."):
                 return_relative_paths = True
-            found_dirs = matches_by_chars(base_dirs, pattern, include_files=include_files)
+
+            # Only include files if the char search is not the last part of the pattern
+            incl = include_files and i == len(path_patterns) - 1
+
+            found_dirs = matches_by_chars(base_dirs, pattern, include_files=incl)
             # Filter out files already found above
             found_dirs = [d for d in found_dirs if d not in found_files]
 
