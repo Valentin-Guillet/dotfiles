67bc6f209541ef56860cdc611946c43ecb575a24
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-10-11 18:12:18 +0200

    Nvim: ignore home git in root specs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,3 +8,19 @@ vim.opt.diffopt = "internal,filler,closeoff,algorithm:histogram,indent-heuristic
 vim.g.autoformat = false
 
 vim.g.snacks_animate = false
+
+-- By default, when searching for the root directory, LazyVim finds "$HOME/.git", but most of
+-- the time we don't want to take it into account so we ignore it here
+---@param patterns string[]|string
+local function no_home_git_pattern_search(buf, patterns)
+	local pattern = require("lazyvim.util.root").detectors.pattern(buf, patterns)
+	return pattern[1] == vim.fn.expand("~") and {} or pattern
+end
+
+vim.g.root_spec = {
+	"lsp",
+	function(buf)
+		return no_home_git_pattern_search(buf, { ".git", "lua" })
+	end,
+	"cwd",
+}
