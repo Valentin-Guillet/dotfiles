362d6b0c97e7bd330025b391950c39331bfea418
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-11-06 09:48:55 +0100

    Python: update history creation

--- .config/python/pythonrc.py
+++ .config/python/pythonrc.py
@@ -1,4 +1,3 @@
-
 import contextlib
 import sys
 
@@ -10,10 +9,10 @@ def setup_history_path():
     import readline
     from pathlib import Path
 
-
     def write_history(history_path):
         import readline
         from pathlib import Path
+
         try:
             Path(history_path).parent.mkdir(parents=True, exist_ok=True)
             readline.write_history_file(history_path)
@@ -21,24 +20,21 @@ def setup_history_path():
             pass
 
     cache_dir = Path(os.environ.get("XDG_CACHE_HOME") or "~/.cache").expanduser()
-    history_file = cache_dir / "python" / "history"
+    history_file = cache_dir / "python" / "history_readline"
 
     readline.set_history_length(10000)
     with contextlib.suppress(FileNotFoundError):
         readline.read_history_file(history_file)
 
-    # Prevents creation of default history if custom is empty
-    if readline.get_current_history_length() == 0:
-        readline.add_history("# History creation")
-
     atexit.register(write_history, history_file)
 
 
 if "ptpython" not in sys.modules:
-    setup_history_path()
+    if sys.version_info < (3, 13):
+        setup_history_path()
 else:
     with contextlib.suppress(ImportError):
-        import pdir as dir     # noqa
+        import pdir as dir  # noqa
 
 
 del contextlib
