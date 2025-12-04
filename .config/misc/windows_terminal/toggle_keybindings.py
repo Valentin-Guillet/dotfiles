bc296982254d3591465b086cb3585ab7bf0c9a23
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-12-04 10:25:47 +0100

    WinTerm: use WSLENV and improve cmd for toggle keybindings

--- .config/misc/windows_terminal/toggle_keybindings.py
+++ .config/misc/windows_terminal/toggle_keybindings.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import shutil
-import subprocess
 from pathlib import Path
 
 
 def get_settings_path():
-    win_path_file = Path.home() / ".cache" / "bash" / "winterm_settings_path"
-
-    # Settings path already saved in cache
-    if win_path_file.exists():
-        with win_path_file.open() as file:
-            return Path(file.read())
-
-    local_app_data = subprocess.check_output(["wslvar", "LOCALAPPDATA"])
-    settings_path = subprocess.check_output(["wslpath", local_app_data]).decode().strip()
-    settings_file = (
-        Path(settings_path) / "Packages" / "Microsoft.WindowsTerminal_8wekyb3d8bbwe" / "LocalState" / "settings.json"
+    settings_path = os.environ["LOCALAPPDATA"]
+    return (
+        Path(settings_path)
+        / "Packages"
+        / "Microsoft.WindowsTerminal_8wekyb3d8bbwe"
+        / "LocalState"
+        / "settings.json"
     )
 
-    with win_path_file.open("w") as file:
-        file.write(str(settings_file))
-
-    return settings_file
 
 def get_custom_settings():
     local_settings_path = Path(__file__).with_name("settings.json")
     with local_settings_path.open() as file:
         return json.load(file)
+
 
 def main():
     settings_file = get_settings_path()
@@ -44,7 +37,9 @@ def main():
         old_settings = json.load(file)
 
     new_settings = old_settings.copy()
-    new_settings["keybindings"] = [obj for obj in new_settings["keybindings"] if not obj.get("toggle", False)]
+    new_settings["keybindings"] = [
+        obj for obj in new_settings["keybindings"] if not obj.get("toggle", False)
+    ]
 
     # If we want to deactivate but no "toggle" has been found, this means that the
     # settings file has been overwritten by WindowsTerminal, so we get all
@@ -53,7 +48,9 @@ def main():
         custom_settings = get_custom_settings()
 
         old_settings["keybindings"] = custom_settings["keybindings"]
-        new_settings["keybindings"] = [obj for obj in custom_settings["keybindings"] if not obj.get("toggle", False)]
+        new_settings["keybindings"] = [
+            obj for obj in custom_settings["keybindings"] if not obj.get("toggle", False)
+        ]
 
     # Save old settings in backup file to reactivate later
     with settings_file_bak.open("w") as file:
