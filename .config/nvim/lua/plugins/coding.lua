e994b42b9877917741fe23bbfa23269d6710661b
Valentin-Guillet <guillet.valentin95240@gmail.com>
2026-03-29 23:05:21 +0200

    Nvim: load mini.comment on keys

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -73,6 +73,13 @@ return {
 
 	{
 		"nvim-mini/mini.comment",
+		-- The "mini.comment" Lazy extra defines event as "VeryLazy"
+		-- We overwrite it so that it is instead loaded on "keys"
+		-- cf. https://www.lazyvim.org/configuration/plugins#%EF%B8%8F-customizing-plugin-specs
+		event = function(_, _)
+			return {}
+		end,
+
 		opts = {
 			options = {
 				ignore_blank_line = true,
@@ -97,7 +104,9 @@ return {
 		},
 		keys = {
 			{
-				"<M-e>", "<Cmd>execute 'Noice dismiss' | lua require('nvim-autopairs.fastwrap').show()<CR>", mode = "i"
+				"<M-e>",
+				"<Cmd>execute 'Noice dismiss' | lua require('nvim-autopairs.fastwrap').show()<CR>",
+				mode = "i",
 			},
 		},
 	},
