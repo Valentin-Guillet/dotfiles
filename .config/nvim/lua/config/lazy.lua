0659d99b22daeec2d808cbe00145549b79797987
Valentin-Guillet <guillet.valentin95240@gmail.com>
2026-03-31 11:21:34 +0200

    Nvim: add LazyVim dotfiles extra

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
+		{ import = "lazyvim.plugins.extras.util.dot" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
